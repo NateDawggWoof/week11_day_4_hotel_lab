afbb97508770638cd5d290fcf179b741f901556d
d7o1d1s0 <d7sarmy@gmail.com>
2021-09-30 14:29:34 +0100

    bookroom function works

--- src/main/java/hotel/Hotel.java
+++ src/main/java/hotel/Hotel.java
@@ -3,8 +3,10 @@ package hotel;
 import hotel.people.guests.Guest;
 import hotel.rooms.Bedroom;
 import hotel.rooms.Room;
+import hotel.system.Booking;
 
 import java.util.ArrayList;
+
 
 public class Hotel {
 
@@ -28,5 +30,11 @@ public class Hotel {
 
     public void checkInGuestToRoom(Guest guest, Bedroom bedroom1) {
         bedroom1.addGuestToRoom(guest);
+    }
+
+    public Booking bookRoom(int numNights, Bedroom bedroom1) {
+        Booking booking;
+        booking = new Booking(numNights, bedroom1);
+        return booking;
     }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -3,6 +3,7 @@ import hotel.people.guests.Guest;
 
 import hotel.rooms.Bedroom;
 import hotel.rooms.RoomType;
+import hotel.system.Booking;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,10 +13,12 @@ public class HotelTest {
     Hotel hotel;
     Bedroom bedroom1;
     Guest guest;
+    Booking booking;
 
     @Before
     public void before(){
         hotel = new Hotel();
+        booking = new Booking(4, bedroom1);
         bedroom1 = new Bedroom(2,5, RoomType.DOUBLE);
         guest = new Guest("Jimmy Bob");
     }
@@ -29,6 +32,18 @@ public class HotelTest {
     public void addGuestToRoom(){
         hotel.checkInGuestToRoom(guest, bedroom1);
         assertEquals(1, bedroom1.countGuests());
+    }
+
+    @Test
+    public void bookRoomHasNumNights(){
+        Booking booking = hotel.bookRoom(4, bedroom1);
+        assertEquals(4, booking.getNumNights());
+    }
+
+    @Test
+    public void bookRoomHasBedroom(){
+        Booking booking = hotel.bookRoom(4, bedroom1);
+        assertEquals(5, booking.getBedroom().getRoomNumber());
     }
 
 }

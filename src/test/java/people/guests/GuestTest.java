ae3c36caf82eb29d3eb980591dda5ebb73422c8a
NateDawggWoof <nathan.pianu@hotmail.co.uk>
2021-09-30 11:24:25 +0100

    All rooms and guests mvp created and tested

--- src/test/java/people/guests/GuestTest.java
+++ src/test/java/people/guests/GuestTest.java
@@ -1,5 +1,22 @@
 package people.guests;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuestTest {
+
+    Guest guest;
+
+    @Before
+    public void before(){
+        guest = new Guest("Jimmy Bob");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Jimmy Bob", guest.getName());
+    }
 
 }
